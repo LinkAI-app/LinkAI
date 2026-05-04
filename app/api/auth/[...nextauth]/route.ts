8cb73f5e0f4c5a9fb22ae00837aa954188518d4a
Ashly Vallecillo <dominikcalderini@Ashlys-MacBook-Air.local>
2026-05-04 18:20:00 -0400

    Fix NextAuth TypeScript error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,27 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      authorization: {
-        params: {
-          scope:
-            "openid email profile https://www.googleapis.com/auth/youtube.upload",
-        },
-      },
     }),
   ],
   callbacks: {
-    async jwt({ token, account }: any) {
+    async jwt({ token, account }: { token: any; account: any }) {
       if (account) {
-        token.accessToken = account.access_token;
+        token.accessToken = account.access_token
       }
-
-      return token;
+      return token
     },
-    async session({ session, token }: any) {
-      session.accessToken = token.accessToken;
-      return session;
+    async session({ session, token }: { session: any; token: any }) {
+      session.accessToken = token.accessToken
+      return session
     },
   },
-};
+}
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions)
 
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST }
