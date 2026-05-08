c594dc05065dabb21ec8e8690c7abf9d1e3e1f28
Ashly Vallecillo <dominikcalderini@Ashlys-MacBook-Air.local>
2026-05-07 22:27:17 -0400

    Fix Stripe webhook

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -8,7 +8,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 export async function POST(req: Request) {
   const body = await req.text();
 
-  const signature = headers().get("stripe-signature") as string;
+  const signature = (await headers()).get(
+    "stripe-signature"
+  ) as string;
 
   let event: Stripe.Event;
 
